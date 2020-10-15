cccdfd935fe74528ed61662aee398dae3c89bd14
Anirudh Jwala <anirudhsai2@gmail.com>
2020-10-15 18:39:28 +0530

    List user details to admin & delete in backend

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,17 +5,20 @@ import {
   registerUser,
   getUserProfile,
   updateUserProfile,
+  getUsers,
+  deleteUser,
 } from "../controllers/userController.js";
 
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/login", authUser);
 router
   .route("/profile")
   .get(protect, getUserProfile)
   .put(protect, updateUserProfile);
+router.route("/:id").delete(protect, admin, deleteUser);
 
 export default router;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -13,6 +13,7 @@ import {
   userRegisterReducer,
   userDetailsReducer,
   userUpdateProfileReducer,
+  userListReducer,
 } from "./reducers/userReducers";
 import {
   orderCreateReducer,
@@ -29,6 +30,7 @@ const reducer = combineReducers({
   userRegister: userRegisterReducer,
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
+  userList: userListReducer,
   orderCreate: orderCreateReducer,
   orderDetails: orderDetailsReducer,
   orderPay: orderPayReducer,
