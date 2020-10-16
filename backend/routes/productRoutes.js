edeefeb45cf4e63755ea052f86d6f2ced97abba1
Anirudh Jwala <anirudhsai2@gmail.com>
2020-10-17 01:34:44 +0530

    Configuring project for deployment

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -9,15 +9,14 @@ import {
   createProductReview,
   getTopProducts,
 } from "../controllers/productController.js";
+
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.route("/top").get(getTopProducts);
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 router.route("/:id/reviews").post(protect, createProductReview);
-
-router.get("/top", getTopProducts);
-
 router
   .route("/:id")
   .get(getProductById)
--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,21 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+const __dirname = path.resolve();
+
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -38,9 +50,6 @@ app.use("/api/upload", uploadRoutes);
 app.get("/api/config/paypal", (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 );
-
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(notFound);
 
